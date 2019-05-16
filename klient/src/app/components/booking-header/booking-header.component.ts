2b318c19ba1c7c8f3b5110b1cff56898ab2e9d16
simpa93 <simon.konstantinovic@forsakringskassan.se>
2019-05-16 20:25:34 +0200

    klick-funktion + ort-val

--- klient/src/app/components/booking-header/booking-header.component.ts
+++ klient/src/app/components/booking-header/booking-header.component.ts
@@ -11,11 +11,40 @@ export class BookingHeaderComponent implements OnInit {
 
   constructor() { }
   user;
+  text1: string;
+  text2: string;
+  text3: string;
 
   ngOnInit() {
     this.user = {
-        name: "Robert"
+        name: "Simon"
       }
+
+      this.text1 = "Sundsvall";
+      this.text2 = "Stockholm";
+      this.text3 = "Karlskrona";
+  }
+
+  onClick1(): void {
+    if(this.text1 == "Sundsvall"){
+      this.text2 = "Sundsvall";
+      this.text1 = "Stockholm";
+    } else if(this.text1 == "Stockholm"){
+      this.text1 = "Sundsvall";
+      this.text2 = "Stockholm";
+    }
+
+    
+  }
+
+  onClick2(): void {
+    if(this.text1 == "Sundsvall"){
+      this.text3 = "Sundsvall";
+      this.text1 = "Karlskrona";
+    } else if(this.text1 == "Karlskrona"){
+      this.text1 = "Sundsvall";
+      this.text3 = "Karlskrona";
+    }
   }
 
 }
