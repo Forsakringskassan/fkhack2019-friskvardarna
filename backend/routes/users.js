b96fa3447d4eba3ed06fde14b6dd69aa0dce75ee
fk-chris <christoffer.nyberg@forsakringskassan.se>
2019-05-16 09:34:00 +0200

    Route for users set up with connection to database

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'fkhack',
+  password: 'oxsta123',
+  port: 5432,
+})
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  pool.query('SELECT * FROM public.users ORDER BY id ASC', (error, results) => {
+    if (error) {
+      throw error
+    }
+    res.status(200).json(results.rows)
+  })
 });
 
 module.exports = router;
